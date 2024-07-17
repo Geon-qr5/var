d6fefbb790285beda48462b96d8c13b7c9bea9ce
Geon-qr5 <zzzqr5@gmail.com>
2024-07-17 09:35:12 +0900

    프로젝트 0717

---
+++ src/com/human/Application.java
@@ -0,0 +1,10 @@
+package com.human;
+
+public class Application {
+    public static void main(String[] args) {
+        com.human.ex01.Ex01 g = new com.human.ex01.Ex01();
+        g.print();
+
+    }
+
+}
---
+++ src/com/human/ex01/Ex01.java
@@ -0,0 +1,16 @@
+package com.human.ex01;
+
+public class Ex01 {
+    public void print(){
+        int i = 2;
+        System.out.println(i + " * 1 = " + ( i * 1 ));
+        System.out.println(i + " * 2 = " + ( i * 2 ));
+        System.out.println(i + " * 3 = " + ( i * 3 ));
+        System.out.println(i + " * 4 = " + ( i * 4 ));
+        System.out.println(i + " * 5 = " + ( i * 5 ));
+        System.out.println(i + " * 6 = " + ( i * 6 ));
+        System.out.println(i + " * 7 = " + ( i * 7 ));
+        System.out.println(i + " * 8 = " + ( i * 8 ));
+        System.out.println(i + " * 9 = " + ( i * 9 ));
+    }
+}
